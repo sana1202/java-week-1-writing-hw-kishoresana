7d285bcb6424f7ba22c28a34b2a9ce29c31c079c
sana1202 <lksana1202@gmail.com>
2023-10-19 10:27:22 +0100

    Practice of Week1 Homework

---
+++ src/week1practice/Programme_7_SumOfTwoNumbers.java
@@ -0,0 +1,16 @@
+package week1practice;
+/**
+ * 7. Write a Java program to print the sum of two numbers.
+ * Test Data: 74 + 36
+ * Expected Output: 110
+ */
+public class Programme_7_SumOfTwoNumbers {
+    public static void main(String[] args) {
+        //The two integers are value 1 and value 2
+        int value1 = 74;
+        int value2 = 36;
+        int sum = value1 + value2;
+        //Concatenation
+        System.out.println(" The Sum of "+ value1 + " and " + value2 + " is " + sum);
+    }
+}
---
+++ src/week1practice/Programme_8_DvisionOfTwoNumbers.java
@@ -0,0 +1,16 @@
+package week1practice;
+/**
+ * 8. Write a Java program to divide two numbers and print on the screen.
+ * Test Data: 50/3
+ * Expected Output: 16
+ */
+public class Programme_8_DvisionOfTwoNumbers {
+    public static void main(String[] args) {
+        //The two integers are value1 and value2
+        int value1 = 50;
+        int value2 = 3;
+        int divison = value1/value2;
+        //Concatenation
+        System.out.println(" The " + value1 + " is divided by " + value2 + " the answer is " + divison );
+    }
+}
